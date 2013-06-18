ece5dc8ba5460fa071d52046e130123662f1ab6a
lnickers <lnickers@gmail.com>
2013-06-17 22:51:09 -0500

    first commit of project files

---
+++ DbContextPreview/AmountExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbContextPreview
+{
+	public partial class Amount
+	{
+		public void OnMaximumAmountChanging()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
